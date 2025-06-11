ac72d2f5bfee149ad0e12dc3ceb3da90c2723738
Sebastian <sebastian.dziadzio@gmail.com>
2025-06-11 18:12:42 -0500

    Fix property name

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def main(cfg: DictConfig):
     merger = create_merge_instance(cfg)
 
     for model in models:
-        download(model.modelId)
-        finetuned_model = AutoModelForCausalLM.from_pretrained(f"models/{model.modelId}")
+        download(model.model_id)
+        finetuned_model = AutoModelForCausalLM.from_pretrained(f"models/{model.model_id}")
         finetuned_model_state_dict = finetuned_model.state_dict()
 
         merged_state_dict = deepcopy(base_model_state_dict)
