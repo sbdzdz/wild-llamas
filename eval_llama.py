89bb610e4d658613749fa76e36e8af64ffce8285
Sebastian <sebastian.dziadzio@gmail.com>
2025-06-17 23:06:45 -0400

    Add an evaluation scrip

---
+++ eval_llama.py
@@ -0,0 +1,31 @@
+from opencompass.models import HuggingFacewithChatTemplate
+from mmengine.config import read_base
+
+with read_base():
+    #from opencompass.configs.dataset_collections.chat_OC15 import datasets
+    #from opencompass.configs.summarizers.chat_OC15 import summarizer
+    from opencompass.configs.datasets.mmlu.mmlu_gen_4d595a import mmlu_datasets
+    from opencompass.configs.datasets.cmmlu.cmmlu_gen_c13365 import cmmlu_datasets
+
+api_meta_template = dict(
+    round=[
+        dict(role='HUMAN', api_role='HUMAN'),
+        dict(role='BOT', api_role='BOT', generate=True),
+    ],
+    reserved_roles=[dict(role='SYSTEM', api_role='SYSTEM')],
+)
+
+models = [
+    dict(
+        type=HuggingFacewithChatTemplate,
+        abbr='merged-llama-3_1-8b-instruct',
+        path='models/merged_model',
+        max_out_len=1024,
+        batch_size=8,
+        run_cfg=dict(num_gpus=1),
+        stop_words=['<|end_of_text|>', '<|eot_id|>'],
+        meta_template=api_meta_template,
+    )
+]
+
+work_dir = 'outputs/merged-llama3-instruct'
