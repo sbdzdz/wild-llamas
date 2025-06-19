ca7e5fd870f63d80a786b6b3d720ac0bdbf70b89
Sebastian <sebastian.dziadzio@gmail.com>
2025-06-19 13:00:34 -0400

    Ensure sufficient memory in the slurm scripts

--- slurm/run_mem.sh
+++ slurm/run_mem.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=h100-ferranti                                              # Partition to use
 #SBATCH --time=3-00:00                                                         # Runtime in D-HH:MM
 #SBATCH --gres=gpu:1                                                           # Request 1 GPU
-#SBATCH --mem=100G                                                             # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=300G                                                             # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/weka/bethge/dziadzio08/wild-llamas/slurm/outputs/%j.out      # File to which STDOUT will be written - make sure this is not on $HOME
 #SBATCH --error=/weka/bethge/dziadzio08/wild-llamas/slurm/outputs/%j.err       # File to which STDERR will be written - make sure this is not on $HOME
 #SBATCH --mail-type=END,FAIL                                                   # Type of email notification- BEGIN,END,FAIL,ALL
